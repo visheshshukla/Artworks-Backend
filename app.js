fc7a23ad639d47ac424eef9c7b0e3b4dc92e3ba4
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-11-27 01:18:50 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const artsRoutes = require('./routes/arts-routes');
+
 const app = express();
+
+app.use(artsRoutes);
 
 app.listen(5000);
