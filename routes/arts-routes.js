896b9d3a4dbe0c15dd2634eadb8b404bac299323
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-12-04 00:21:08 +0530

    updated arts-routes.js

--- routes/arts-routes.js
+++ routes/arts-routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { check } = require('express-validator');
 
 const router = express.Router();
 
@@ -8,9 +9,30 @@ router.get('/:aid', artsControllers.getArtById);
 
 router.get('/user/:uid', artsControllers.getArtsByUserId);
 
-router.post('/', artsControllers.createArt);
-
-router.patch('/:pid', artsControllers.updateArt);
+router.post(
+    '/',
+    [
+      check('title')
+        .not()
+        .isEmpty(),
+      check('description').isLength({ min: 5 }),
+      check('address')
+        .not()
+        .isEmpty()
+    ],
+    artsControllers.createArt
+  );
+  
+  router.patch(
+    '/:pid',
+    [
+      check('title')
+        .not()
+        .isEmpty(),
+      check('description').isLength({ min: 5 })
+    ],
+    artsControllers.updateArt
+  );
 
 router.delete('/:pid', artsControllers.deleteArt);
 
