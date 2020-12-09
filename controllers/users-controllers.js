78f67c0a4913d3e16433ffd6f9b9dae1d086114f
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-12-10 00:05:18 +0530

    updated users-controllers.js

--- controllers/users-controllers.js
+++ controllers/users-controllers.js
@@ -2,43 +2,59 @@ const { v4: uuidv4 } = require('uuid');
 const { validationResult } = require('express-validator');
 
 const HttpError = require('../models/http-error');
-
-const DUMMY_USERS = [
-  {
-    id: 'u1',
-    name: 'Vishesh Shukla',
-    email: 'test@test.com',
-    password: 'test123'
-  }
-];
+const User = require('../models/user');
 
 const getUsers = (req, res, next) => {
   res.json({ users: DUMMY_USERS });
 };
 
-const signup = (req, res, next) => {
+const signup = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
-    throw new HttpError('Invalid inputs passed, please check your data.', 422);
+    return next(
+      new HttpError('Invalid inputs passed, please check your data.', 422)
+    );
   }
+  const { name, email, password, arts } = req.body;
 
-  const { name, email, password } = req.body;
-
-  const hasUser = DUMMY_USERS.find(u => u.email === email);
-  if (hasUser) {
-    throw new HttpError('Could not create user, email already exists.', 422);
+  let existingUser
+  try {
+    existingUser = await User.findOne({ email: email })
+  } catch (err) {
+    const error = new HttpError(
+      'Signing up failed, please try again later 1.',
+      500
+    );
+    return next(error);
   }
-
-  const createdUser = {
-    id: uuidv4(),
-    name, 
+  
+  if (existingUser) {
+    const error = new HttpError(
+      'User exists already, please login instead 2.',
+      422
+    );
+    return next(error);
+  }
+  
+  const createdUser = new User({
+    name,
     email,
-    password
-  };
+    image: 'https://res.cloudinary.com/vishesh123/image/upload/v1589820758/avatar_ncyywy.png',
+    password,
+    arts
+  });
 
-  DUMMY_USERS.push(createdUser);
+  try {
+    await createdUser.save();
+  } catch (err) {
+    const error = new HttpError(
+      'Signing up failed, please try again 3.',
+      500
+    );
+    return next(error);
+  }
 
-  res.status(201).json({user: createdUser});
+  res.status(201).json({user: createdUser.toObject({ getters: true })});
 };
 
 const login = (req, res, next) => {
