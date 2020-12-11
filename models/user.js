8f10ddbaae31e5efe9fa9d59f35d5561eaf73dc1
Vishesh Shukla <engr.visheshshukla@gmail.com>
2020-12-12 00:46:49 +0530

    updated user.js

--- models/user.js
+++ models/user.js
@@ -9,7 +9,7 @@ const userSchema = new Schema({
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true, minlength: 6 },
     image: { type: String, required: true },
-    arts: { type: String, required: true },
+    arts: [{ type: mongoose.Types.ObjectId, required: true, ref: 'Art' }]
 });
 
 userSchema.plugin(uniqueValidator);
