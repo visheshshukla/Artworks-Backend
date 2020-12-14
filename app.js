938dd666381a3e11daabe91a3e66cb8f9c4cf268
visheshshukla <engr.visheshshukla@gmail.com>
2020-12-15 02:55:41 +0530

    updated app.js

--- app.js
+++ app.js
@@ -14,6 +14,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PATCH, DELETE');
+  next();
+});
+
 app.use('/api/arts', artsRoutes);
 app.use('/api/users', usersRoutes);
 
