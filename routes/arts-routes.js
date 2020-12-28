3a8b0eb4d28b1e3630a5cf3df5d792ed12c74ffc
visheshshukla <engr.visheshshukla@gmail.com>
2020-12-29 00:38:20 +0530

    updated arts-routes.js

--- routes/arts-routes.js
+++ routes/arts-routes.js
@@ -5,10 +5,13 @@ const router = express.Router();
 
 const artsControllers = require('../controllers/arts-controllers');
 const fileUpload = require('../middleware/file-upload');
+const checkAuth = require('../middleware/check-auth');
 
 router.get('/:aid', artsControllers.getArtById);
 
 router.get('/user/:uid', artsControllers.getArtsByUserId);
+
+router.use(checkAuth);
 
 router.post(
     '/',
